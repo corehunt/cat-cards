be03cdf070712a1fe1ee5a55d42358b03309760f
Reshmi Joseph <reshmijosephk@gmail.com>
2021-11-01 15:41:39 -0400

    Added URLs to RestService classes

--- src/main/java/com/techelevator/services/RestCatFactService.java
+++ src/main/java/com/techelevator/services/RestCatFactService.java
@@ -1,26 +1,25 @@
 package com.techelevator.services;
 
 import org.springframework.stereotype.Component;
-
 import com.techelevator.model.CatFact;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RestCatFactService implements CatFactService {
 
-	private String API_BASE_URL = "https://cat-data.netlify.app/api/facts/random";
-	private String API_KEY = "3051";
+	private String API_URL = "https://cat-data.netlify.app/api/facts/random";
+//	private String API_KEY = "3051";
 	private RestTemplate restTemplate = new RestTemplate();
 
-	public RestCatFactService(String apiURL, String apiKey) {
-		API_BASE_URL = apiURL;
-		API_KEY = apiKey;
-	}
+//	public RestCatFactService(String apiURL) {
+//		API_URL = apiURL;
+//	//	API_KEY = apiKey;
+//	}
 
 	@Override
 	public CatFact getFact() {
 		// TODO Auto-generated method stub
-		return restTemplate.getForObject(API_BASE_URL + API_KEY, CatFact.class);
+		return restTemplate.getForObject(API_URL , CatFact.class);
 	}
 
 }
--- src/main/java/com/techelevator/services/RestCatPicService.java
+++ src/main/java/com/techelevator/services/RestCatPicService.java
@@ -1,16 +1,26 @@
 package com.techelevator.services;
 
+import com.techelevator.model.CatFact;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.model.CatPic;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RestCatPicService implements CatPicService {
 
+
+
+
+	private String API_URL = "https://cat-data.netlify.app/api/pictures/random";
+
+	private RestTemplate restTemplate = new RestTemplate();
+
+
 	@Override
 	public CatPic getPic() {
 		// TODO Auto-generated method stub
-		return null;
+		return restTemplate.getForObject(API_URL , CatPic.class);
 	}
 
 }	
